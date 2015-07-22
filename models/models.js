50e03608c88b2874f477c07b0e6d3bb306c20607
the_answer_forn <the.answer.forn@gmail.com>
2015-07-22 14:26:51 +0000

    Lista de preguntas

--- models/models.js
+++ models/models.js
@@ -36,10 +36,11 @@ exports.Quiz = Quiz; // exportar tabla Quiz
 // sequelize.sync() inicializa tabla de preguntas en DB
 sequelize.sync().then(function() {
   // then(..) ejecuta el manejador una vez creada la tabla
-  Quiz.count().then(function (count){    if(count === 0) {   // la tabla se inicializa solo si está vacía
+  Quiz.count().then(function (count){    
+    if(count === 0) {   // la tabla se inicializa solo si está vacía
      Quiz.bulkCreate( 
         [ {pregunta: 'Capital de Italia',   respuesta: 'Roma'},
-          {pregunta: 'Capital de Portugal', respuesta: 'Lisboa'}
+          {pregunta: 'Capital de Portugal', respuesta: 'Lisboa'},
         ]
       ).then(function(){console.log('Base de datos inicializada')});
 
