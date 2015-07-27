a171c82ab609ec77566a74a3c93f9c90d7138486
the_answer_forn <the.answer.forn@gmail.com>
2015-07-27 16:39:17 +0000

    Autoload

--- models/models.js
+++ models/models.js
@@ -40,7 +40,7 @@ sequelize.sync().then(function() {
     if(count === 0) {   // la tabla se inicializa solo si está vacía
      Quiz.bulkCreate( 
         [ {pregunta: 'Capital de Italia',   respuesta: 'Roma'},
-          {pregunta: 'Capital de Portugal', respuesta: 'Lisboa'},
+          {pregunta: 'Capital de Portugal', respuesta: 'Lisboa'}
         ]
       ).then(function(){console.log('Base de datos inicializada')});
 
